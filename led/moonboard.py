9b012ab164d75a0659da9a1bc1adb119d6cece09
Kevin Hew <kevin.hew@gmail.com>
2023-08-27 22:17:21 +0200

    change colours

--- led/moonboard.py
+++ led/moonboard.py
@@ -12,7 +12,7 @@ import time
 import os
 
 class MoonBoard:
-    DEFAULT_PROBLEM_COLORS = {'START':COLORS.blue,'TOP':COLORS.red,'MOVES':COLORS.green}
+    DEFAULT_PROBLEM_COLORS = {'START':COLORS.green,'TOP':COLORS.red,'MOVES':COLORS.blue}
     DEFAULT_COLOR = COLORS.blue #FIXME ?
     X_GRID_NAMES = string.ascii_uppercase[0:11] # FIXME: del
     ROWS = 18 
